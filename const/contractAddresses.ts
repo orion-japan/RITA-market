ae8616c84b5e126861cb4d3a3100a9053b046ccd
orion-japan <arigatousun8@gmail.com>
2023-03-19 14:51:16 +0900

    first commit

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -2,8 +2,8 @@
 
 // 1. Set up the network your smart contracts are deployed to.
 // First, import the chain from the package, then set the NETWORK variable to the chain.
-import { Mumbai } from "@thirdweb-dev/chains";
-export const NETWORK = Mumbai;
+import { ThirdwebProvider } from "@thirdweb-dev/chains";
+export const NETWORK = ThirdwebProvider;
 
 // 2. The address of the marketplace V3 smart contract.
 // Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
